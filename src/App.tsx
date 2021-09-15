82570cdddfaf70bf827c784900ec95f14b0fa082
Toàn Trần <toantran3110@gmail.com>
2021-09-15 22:28:49 +0700

    add more code

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Snacker APP from Toan Tran
+          Welcome to Snacker APP from Toan Tran 2
         </p>
         <a
           className="App-link"
